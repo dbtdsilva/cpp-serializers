b28b3de97900725e9e9cda8809b2c4ad0c673462
Diogo Silva <dbtds@ua.pt>
2016-11-07 17:02:01 +0000

    Slightly modified feature-test to ignore some fields and cleanup

--- feature-test/feature-test.cpp
+++ feature-test/feature-test.cpp
@@ -24,41 +24,25 @@ int main(int argc, char **argv)
 
     vector<unique_ptr<FeatureTestObject>> tests_protocols;
 
-    if (argc < 2) {
-        std::cout << "usage: " << argv[0] << " N [thrift-binary thrift-compact protobuf boost msgpack cereal avro capnproto flatbuffers bson]";
+    if (argc < 1) {
+        std::cout << "usage: " << argv[0] << " [thrift-binary thrift-compact protobuf boost msgpack cereal avro capnproto flatbuffers bson]";
         std::cout << std::endl << std::endl;
-        std::cout << "arguments: " << std::endl;
-        std::cout << " N  -- number of iterations" << std::endl << std::endl;
         return EXIT_SUCCESS;
     }
 
-    size_t iterations;
-
-    try {
-        iterations = boost::lexical_cast<size_t>(argv[1]);
-    } catch (std::exception &exc) {
-        std::cerr << "Error: " << exc.what() << std::endl;
-        std::cerr << "First positional argument must be an integer." << std::endl;
-        return EXIT_FAILURE;
-    }
-
     std::set<std::string> names;
-
     if (argc > 2) {
         for (int i = 2; i < argc; i++) {
             names.insert(argv[i]);
         }
     }
 
-    std::cout << "performing " << iterations << " iterations" << std::endl << std::endl;
-
-    /*std::cout << "total size: " << sizeof(kIntegerValue) * kIntegersCount + kStringValue.size() * kStringsCount << std::endl;*/
-
     try {
         if (names.empty() || names.find("thrift-binary") != names.end()) {
         }
 
         if (names.empty() || names.find("thrift-compact") != names.end()) {
+
         }
 
         if (names.empty() || names.find("protobuf") != names.end()) {
@@ -92,7 +76,11 @@ int main(int argc, char **argv)
 
     for(auto const& protocol: tests_protocols) {
         cout << "Testing " << protocol->get_name() << endl;
-
+        cout << "\tMissing field: " << (protocol->check_missing_field() ? "PASS" : "FAILED") << endl;
+        cout << "\tAdded new field: " << (protocol->check_new_field() ? "PASS" : "FAILED") << endl;
+        cout << "\tRenaming fields: " << (protocol->check_field_names() ? "PASS" : "FAILED") << endl;
+        cout << "\tType inheritance: " << (protocol->check_types_inheritance() ? "PASS" : "FAILED") << endl;
+        cout << endl;
     }
 
     google::protobuf::ShutdownProtobufLibrary();
