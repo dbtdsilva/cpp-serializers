1f255ce3adb8ddf44e5c8419c4551eceb43419e5
Diogo Silva <dbtds@ua.pt>
2016-11-09 23:19:08 +0000

    Added Avro feature test

--- feature-test/avro/avro.cpp
+++ feature-test/avro/avro.cpp
@@ -1,24 +1,119 @@
 #include "avro.h"
-#include <string>
+#include <iostream>
+#include <avro/Exception.hh>
+#include "schema/record.h"
+#include "../data.hpp"
 
 using namespace std;
+using namespace avro;
 
 AvroTest::AvroTest() : FeatureTestObject(string("Avro")) {
 
 }
 
 bool AvroTest::check_missing_field() {
-    return false;
+    Record r1;
+    for (size_t i = 0; i < kIntegers.size(); i++) {
+        r1.ids.push_back(kIntegers[i]);
+    }
+    for (size_t i = 0; i < kStringsCount; i++) {
+        r1.strings.push_back(kStringValue);
+    }
+
+    std::unique_ptr<OutputStream> out = memoryOutputStream();
+    EncoderPtr encoder = binaryEncoder();
+
+    encoder->init(*out);
+    encode(*encoder, r1);
+
+    std::unique_ptr<InputStream> in = memoryInputStream(*out);
+    DecoderPtr decoder = binaryDecoder();
+
+    decoder->init(*in);
+    RecordMissing r2;
+    decode(*decoder, r2);
+
+    return !(r1.ids != r2.ids || r2.ids.size() != kIntegers.size());
 }
 
 bool AvroTest::check_new_field() {
-    return false;
+    Record r1;
+    for (size_t i = 0; i < kIntegers.size(); i++) {
+        r1.ids.push_back(kIntegers[i]);
+    }
+    for (size_t i = 0; i < kStringsCount; i++) {
+        r1.strings.push_back(kStringValue);
+    }
+
+    std::unique_ptr<OutputStream> out = memoryOutputStream();
+    EncoderPtr encoder = binaryEncoder();
+
+    encoder->init(*out);
+    encode(*encoder, r1);
+
+    std::unique_ptr<InputStream> in = memoryInputStream(*out);
+    DecoderPtr decoder = binaryDecoder();
+
+    decoder->init(*in);
+    RecordNewField r2;
+    decode(*decoder, r2);
+
+    return !(r1.ids != r2.ids || r2.ids.size() != kIntegers.size() || r1.strings != r2.strings ||
+            r2.strings.size() != kStringsCount);
 }
 
 bool AvroTest::check_types_inheritance() {
-    return false;
+    Record r1;
+    for (size_t i = 0; i < kIntegers.size(); i++) {
+        r1.ids.push_back(kIntegers[i]);
+    }
+    for (size_t i = 0; i < kStringsCount; i++) {
+        r1.strings.push_back(kStringValue);
+    }
+
+    std::unique_ptr<OutputStream> out = memoryOutputStream();
+    EncoderPtr encoder = binaryEncoder();
+
+    encoder->init(*out);
+    encode(*encoder, r1);
+
+    std::unique_ptr<InputStream> in = memoryInputStream(*out);
+    DecoderPtr decoder = binaryDecoder();
+
+    decoder->init(*in);
+    RecordTypeInheritance r2;
+    try {
+        decode(*decoder, r2);
+    } catch (avro::Exception&) {
+        return false;
+    }
+
+    return !(r2.ids.size() != kIntegers.size() || r1.strings != r2.strings ||
+             r2.strings.size() != kStringsCount);
 }
 
 bool AvroTest::check_field_names() {
-    return false;
+    Record r1;
+    for (size_t i = 0; i < kIntegers.size(); i++) {
+        r1.ids.push_back(kIntegers[i]);
+    }
+    for (size_t i = 0; i < kStringsCount; i++) {
+        r1.strings.push_back(kStringValue);
+    }
+
+    std::unique_ptr<OutputStream> out = memoryOutputStream();
+    EncoderPtr encoder = binaryEncoder();
+
+    encoder->init(*out);
+    encode(*encoder, r1);
+
+    std::unique_ptr<InputStream> in = memoryInputStream(*out);
+    DecoderPtr decoder = binaryDecoder();
+
+    decoder->init(*in);
+    RecordRename r2;
+    decode(*decoder, r2);
+
+    return !(r1.ids != r2.ids_rem || r2.ids_rem.size() != kIntegers.size() || r1.strings != r2.strings_rem ||
+             r2.strings_rem.size() != kStringsCount);
 }
