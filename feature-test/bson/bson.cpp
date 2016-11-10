5d0d12cc2b1d1fdd8fdeef2c50d2b96ef9180e7f
Diogo Silva <dbtds@ua.pt>
2016-11-10 01:09:49 +0000

    Added BSON test

--- feature-test/bson/bson.cpp
+++ feature-test/bson/bson.cpp
@@ -1,6 +1,6 @@
 #include "bson.h"
-#include <string>
 #include <bson.h>
+#include <iostream>
 
 #include "../data.hpp"
 
@@ -26,31 +26,173 @@ bool BsonTest::check_missing_field() {
         bson_append_utf8(&strings, "0", -1, kStringValue.c_str(), kStringValue.size());
     bson_append_array_end(&bson, &strings);
 
-    if (bson_iter_init_find (&iter, &bson, "ints") &&
-        (BSON_ITER_HOLDS_DOCUMENT (&iter) || BSON_ITER_HOLDS_ARRAY (&iter)) &&
-        bson_iter_recurse (&iter, &sub_iter)) {
-        bson_iter_next (&sub_iter);
-        bson_iter_int32(&sub_iter);
-    }
+    unsigned int counter = 0;
+    const char * recvString;
     if (bson_iter_init_find (&iter, &bson, "strings") &&
         (BSON_ITER_HOLDS_DOCUMENT (&iter) || BSON_ITER_HOLDS_ARRAY (&iter)) &&
         bson_iter_recurse (&iter, &sub_iter)) {
-        bson_iter_next (&sub_iter);
-        bson_iter_utf8(&sub_iter, NULL);
+        while (bson_iter_next (&sub_iter)) {
+            recvString = bson_iter_utf8(&sub_iter, NULL);
+            if (string(recvString) != kStringValue)
+                return false;
+            counter++;
+        }
     }
-    bson_destroy(&bson);
+    if (counter != kStringsCount)
+        return false;
 
+    bson_destroy(&bson);
     return true;
 }
 
 bool BsonTest::check_new_field() {
-    return false;
+    bson_t bson, ints, strings;
+    bson_init(&bson);
+    bson_iter_t iter;
+    bson_iter_t sub_iter;
+
+    bson_append_int64(&bson, "int", -1, 10);
+    bson_append_array_begin(&bson, "ints", -1, &ints);
+    for (size_t i = 0; i < kIntegers.size(); i++)
+        bson_append_int32(&ints, "0", -1, kIntegers[i]);
+    bson_append_array_end(&bson, &ints);
+
+    bson_append_array_begin(&bson, "strings", -1, &strings);
+    for (size_t i = 0; i < kStringsCount; i++)
+        bson_append_utf8(&strings, "0", -1, kStringValue.c_str(), kStringValue.size());
+    bson_append_array_end(&bson, &strings);
+
+    unsigned int counter = 0;
+    int recvInt;
+    if (bson_iter_init_find(&iter, &bson, "ints") &&
+        (BSON_ITER_HOLDS_DOCUMENT (&iter) || BSON_ITER_HOLDS_ARRAY (&iter)) &&
+        bson_iter_recurse (&iter, &sub_iter)) {
+        while (bson_iter_next (&sub_iter)) {
+            recvInt = bson_iter_int32(&sub_iter);
+            if (kIntegers.at(counter) != recvInt)
+                return false;
+            counter++;
+        }
+    }
+    if (counter != kIntegers.size())
+        return false;
+
+    counter = 0;
+    const char * recvString;
+    if (bson_iter_init_find (&iter, &bson, "strings") &&
+        (BSON_ITER_HOLDS_DOCUMENT (&iter) || BSON_ITER_HOLDS_ARRAY (&iter)) &&
+        bson_iter_recurse (&iter, &sub_iter)) {
+        while (bson_iter_next (&sub_iter)) {
+            recvString = bson_iter_utf8(&sub_iter, NULL);
+            if (string(recvString) != kStringValue)
+                return false;
+            counter++;
+        }
+    }
+
+    if (counter != kStringsCount)
+        return false;
+
+    bson_destroy(&bson);
+    return true;
 }
 
 bool BsonTest::check_types_inheritance() {
-    return false;
+    bson_t bson, ints, strings;
+    bson_init(&bson);
+    bson_iter_t iter;
+    bson_iter_t sub_iter;
+
+    bson_append_array_begin(&bson, "ints", -1, &ints);
+    for (size_t i = 0; i < kIntegers.size(); i++)
+        bson_append_double(&ints, "0", -1, kIntegers[i]);
+    bson_append_array_end(&bson, &ints);
+
+    bson_append_array_begin(&bson, "strings", -1, &strings);
+    for (size_t i = 0; i < kStringsCount; i++)
+        bson_append_utf8(&strings, "0", -1, kStringValue.c_str(), kStringValue.size());
+    bson_append_array_end(&bson, &strings);
+
+    unsigned int counter = 0;
+    int recvInt;
+    if (bson_iter_init_find (&iter, &bson, "ints") &&
+        (BSON_ITER_HOLDS_DOCUMENT (&iter) || BSON_ITER_HOLDS_ARRAY (&iter)) &&
+        bson_iter_recurse (&iter, &sub_iter)) {
+        while (bson_iter_next (&sub_iter)) {
+            recvInt = bson_iter_int32(&sub_iter);
+            if (kIntegers.at(counter) != recvInt)
+                return false;
+            counter++;
+        }
+    }
+    if (counter != kIntegers.size())
+        return false;
+
+    counter = 0;
+    const char * recvString;
+    if (bson_iter_init_find (&iter, &bson, "strings") &&
+        (BSON_ITER_HOLDS_DOCUMENT (&iter) || BSON_ITER_HOLDS_ARRAY (&iter)) &&
+        bson_iter_recurse (&iter, &sub_iter)) {
+        while (bson_iter_next (&sub_iter)) {
+            recvString = bson_iter_utf8(&sub_iter, NULL);
+            if (string(recvString) != kStringValue)
+                return false;
+            counter++;
+        }
+    }
+    if (counter != kStringsCount)
+        return false;
+
+    bson_destroy(&bson);
+    return true;
 }
 
 bool BsonTest::check_field_names() {
-    return false;
+    bson_t bson, ints, strings;
+    bson_init(&bson);
+    bson_iter_t iter;
+    bson_iter_t sub_iter;
+
+    bson_append_array_begin(&bson, "ints_rem", -1, &ints);
+    for (size_t i = 0; i < kIntegers.size(); i++)
+        bson_append_int32(&ints, "0", -1, kIntegers[i]);
+    bson_append_array_end(&bson, &ints);
+
+    bson_append_array_begin(&bson, "strings_rem", -1, &strings);
+    for (size_t i = 0; i < kStringsCount; i++)
+        bson_append_utf8(&strings, "0", -1, kStringValue.c_str(), kStringValue.size());
+    bson_append_array_end(&bson, &strings);
+
+    unsigned int counter = 0;
+    int recvInt;
+    if (bson_iter_init_find (&iter, &bson, "ints") &&
+        (BSON_ITER_HOLDS_DOCUMENT (&iter) || BSON_ITER_HOLDS_ARRAY (&iter)) &&
+        bson_iter_recurse (&iter, &sub_iter)) {
+        while (bson_iter_next (&sub_iter)) {
+            recvInt = bson_iter_int32(&sub_iter);
+            if (kIntegers.at(counter) != recvInt)
+                return false;
+            counter++;
+        }
+    }
+    if (counter != kIntegers.size())
+        return false;
+
+    counter = 0;
+    const char * recvString;
+    if (bson_iter_init_find (&iter, &bson, "strings") &&
+        (BSON_ITER_HOLDS_DOCUMENT (&iter) || BSON_ITER_HOLDS_ARRAY (&iter)) &&
+        bson_iter_recurse (&iter, &sub_iter)) {
+        while (bson_iter_next (&sub_iter)) {
+            recvString = bson_iter_utf8(&sub_iter, NULL);
+            if (string(recvString) != kStringValue)
+                return false;
+            counter++;
+        }
+    }
+    if (counter != kStringsCount)
+        return false;
+
+    bson_destroy(&bson);
+    return true;
 }
