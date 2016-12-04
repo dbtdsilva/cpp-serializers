14b36ead1422032e4bb6a397db71651c55d2b961
Diogo Silva <dbtds@ua.pt>
2016-12-04 14:50:23 +0000

    Updated script

--- benchmark_full_test.sh
+++ benchmark_full_test.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-data_size=70000 	# 70000 * (8 + 8) = 1.2 MB
+if [ -z "$1" ]
+then
+	echo "Usage: $0 output_file"
+	exit 1
+fi
+
+data_size=700000 	# 70000 * (8 + 8) = 1.2 MB
 data_sequence=1000
 data_interval=$((data_size/data_sequence))
 
 current_data_request=1
 index=0
 
-iterations=1000
+iterations=100000
 while [ $current_data_request -lt $data_size ]
 do
-	printf "\n\n>> Test incoming with a data size of " $current_data_request
+	echo ">> Test incoming with a data size of $current_data_request" >> $1
+	echo ">> $index of $data_sequence tests" | tee -a $1
 	SECONDS=0
-	./benchmark $iterations $current_data_request
+	./benchmark $iterations $current_data_request >> $1
 	duration=$SECONDS
 
 	if [ $duration -lt 2 ]
@@ -20,11 +27,13 @@ do
 		iterations=$((iterations*10))
 	fi
 
-	if [ $duration -gt 10 ]
+	if [ $duration -gt 6 ]
 	then
-		iterations=$((iterations/10))
+		iterations=$((iterations/5))
 	fi
 
 	index=$((index+1))
 	current_data_request=$((index*data_interval))
 done
+
+echo ">> Tests have finished"
